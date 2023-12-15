ebceadff33ef8fe70f77de3287bc49ed0ae415f3
Amanuniqueboy <126694047+Amanuniqueboy@users.noreply.github.com>
2023-12-16 00:27:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 26350004
 API_HASH     = "4fc590a80c2e2ccec3587cb1568e8f9e"
 BOT_TOKEN    = "6858786656:AAGTqreNKkmIUXFcX0N6icWk0LjmDj1hD1s"
-SESSION      = "BQEVDDgAqAeqpwh6ISGOHhApFeAW2aOQ1fgvqNm5kH5dR2DFH3AGfjSAG6gtsq4Mz2Ra6uekSou5X1Breuqjs-DTnHtHNfRVDj3RB86iRFlIF1mA7R4kHDvGF-qVFGMbUjePCSFarxFZch-AHNN4SspYH_i3KHN5h_juhSOSlT5O2iVGIq2CNp22_aPxWkifsBrQnUuyJ3tKTy7_HQ734ICQ5RATZtWtMdO0kCUbY6TyhgYJ0L8jm7AnsqnEmBdM2UkF4fuuasDrOmDocrm39vbITKNgjnucAODx2AtY8bSD5qNDx-BH_vIORzuGF72wqzU19kDzlpyb_-Uqx483blPHyKfOQgAAAAFhSwkGAA"
-DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001980857896
-ADMIN        = 6051691339
+SESSION      = "BQE81YQAof3cDbOu3TF-pty2JagSUQOIUaXUATRxOcd6grQDlQZvM3xwrPqiIxP6dzYaA4cWRfgIv5sGGtgF3Bt5ootWH3ttF8fcjn4Ydn7dPRcYtASOy5Lkm2p9FstU7-cVIZeWMLsGvBZIqYpiFF8qcDXFBmpmYxFpWY2TOlPm7lh5FLO58bVsACTLW6kTO18in8noeCA3vaeyMto5xAw4mlmXs1oec0_G322xiQpM6j8D-txfU-YcZh7OPnjUvOctQg-62vPlnq_jQ2v_-8uhbERXPEc76h5k4u6xXGX2vzYNg660RlcB_QTgsB8kMq44tnYzyegwS8vPv_zBNDi2uin34QAAAAFPDYYqAA"
+DATABASE_URI = "mongodb+srv://faheem:faheem@cluster0.kf22sgm.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1002068213132
+ADMIN        = 5621253674
