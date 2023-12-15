6a06761dc066fa9c730276a25dcf45961ba31b1f
Amanuniqueboy <126694047+Amanuniqueboy@users.noreply.github.com>
2023-12-16 01:43:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ API_ID       = 26350004
 API_HASH     = "4fc590a80c2e2ccec3587cb1568e8f9e"
 BOT_TOKEN    = "6858786656:AAGTqreNKkmIUXFcX0N6icWk0LjmDj1hD1s"
 SESSION      = "BQE81YQAof3cDbOu3TF-pty2JagSUQOIUaXUATRxOcd6grQDlQZvM3xwrPqiIxP6dzYaA4cWRfgIv5sGGtgF3Bt5ootWH3ttF8fcjn4Ydn7dPRcYtASOy5Lkm2p9FstU7-cVIZeWMLsGvBZIqYpiFF8qcDXFBmpmYxFpWY2TOlPm7lh5FLO58bVsACTLW6kTO18in8noeCA3vaeyMto5xAw4mlmXs1oec0_G322xiQpM6j8D-txfU-YcZh7OPnjUvOctQg-62vPlnq_jQ2v_-8uhbERXPEc76h5k4u6xXGX2vzYNg660RlcB_QTgsB8kMq44tnYzyegwS8vPv_zBNDi2uin34QAAAAFPDYYqAA"
-DATABASE_URI = "mongodb+srv://faheem:faheem@cluster0.kf22sgm.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://faheem:faheem@cluster0.ixh5rtx.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1002068213132
 ADMIN        = 5621253674
